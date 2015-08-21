027a4493fc3fab881e274b6f7636c5923a623d4b
blecman <snowyrahul@gmail.com>
2015-08-21 18:38:25 -0400

    update repl to be able to handle nested loops

--- httplang/repl.py
+++ httplang/repl.py
@@ -2,10 +2,20 @@ import utils
 import parse
 
 def enterREPL():
+    
+    #loop state code
+    loopStatus = {'loopDepth':0,
+                  'rootLoopLength':0,
+                  'loopCode':[]}
+    
     while(True):
         inp = raw_input(str(utils.lines) + "> ").strip()
 
         if(inp == "quit"):
             return
-
-        parse.parse(inp)
+        
+        #update line count
+        utils.lines += 1
+        
+        #handles loops and executes otherwise    
+        loopStatus = parse.loopCheckThenParse(utils.lines,inp,loopStatus)
